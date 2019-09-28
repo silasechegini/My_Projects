b599ac59b073bfaca101416c45f489585fbe09a6
silasngozi <silasngoziechegini@gmail.com>
2019-09-28 00:44:30 -0400

    renamed data sets

--- merge_sort.py
+++ merge_sort.py
@@ -1,17 +1,17 @@
 #  The aim of this code is to sort the elements of an array in a more 
 # efficient way that is not the bubble sort, rather, the merge sort.
 
-items = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
+data = [12, 50, 18, 21, 65, 31, 70, 14, 94, 67]
 
-def mergeSort(dataset):
-    if len(dataset) > 1:
-        mid = len(dataset) // 2
-        lefttarr = dataset[:mid]
-        rightarr = dataset[mid:]
+def merge_Sort(dataclass):
+    if len(dataclass) > 1:
+        mid = len(dataclass) // 2
+        left_array = dataclass[:mid]
+        right_array = dataclass[mid:]
 
         # now, recursively reduce the array size to one
-        mergeSort(lefttarr)
-        mergeSort(rightarr)
+        merge_Sort(left_array)
+        merge_Sort(right_array)
 
         # now, begin the merge operation
         i = 0 #index for the left array
@@ -19,25 +19,25 @@ def mergeSort(dataset):
         k = 0 #index for the merged array
 
         # while each array has content
-        while i < len(lefttarr) and j < len(rightarr):
-            if lefttarr[i] < rightarr[j]:
-                dataset[k] = lefttarr[i]
+        while i < len(left_array) and j < len(right_array):
+            if left_array[i] < right_array[j]:
+                dataclass[k] = left_array[i]
                 i += 1
             else:
-                dataset[k] = rightarr[j]
+                dataclass[k] = right_array[j]
                 j += 1
             k += 1
 
-        # now, add what ever is left of both arrays to the dataset
-        while i < len(lefttarr):
-            dataset[k] = lefttarr[i]
+        # now, add what ever is left of both arrays to the dataclass
+        while i < len(left_array):
+            dataclass[k] = left_array[i]
             i += 1
             k += 1
-        while j < len(rightarr):
-            dataset[k] = rightarr[j]
+        while j < len(right_array):
+            dataclass[k] = right_array[j]
             j += 1
             k += 1
 
-print(items)
-mergeSort(items)
-print(items)
+print(data)
+merge_Sort(data)
+print(data)
